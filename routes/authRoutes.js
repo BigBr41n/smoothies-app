95fe7e4259fef6cbea1a10ec58bf9cdf702701e4
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-03-28 02:44:57 +0100

    auth controller

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,17 @@
 //creating a new instance of the router
 const router = require('express').Router(); 
 
+//importing auth controller 
+const authController = require('../controllers/authController'); 
 
 
 
-router.get('/signup' , ()=>{});
-router.post('/signup' , ()=>{});
-router.get('/login' , ()=>{});
-router.post('/login' , ()=>{});
+
+router.get('/signup' , authController.signUpGet );
+router.post('/signup' , authController.signUpPost);
+router.get('/login' , authController.loginGet);
+router.post('/login' , authController.loginPost);
+
 
 
 
