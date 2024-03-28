ca9f359013b2ca8e1468ae7e0abdff35b0f4d869
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-03-28 03:51:22 +0100

    error handlling function

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,46 @@
 const User = require('../models/User'); 
 const bcrypt = require('bcrypt'); 
 
+
+
+// handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password: '' };
+  
+    // duplicate email error
+    if (err.code === 11000) {
+      errors.email = 'that email is already registered';
+      return errors;
+    }
+  
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+      // console.log(err);
+      Object.values(err.errors).forEach(({ properties }) => {
+        // console.log(val);
+        // console.log(properties);
+        errors[properties.path] = properties.message;
+      });
+    }
+  
+    return errors;
+  }
+
+
+
+
+
+
+
+
+
+
+
 module.exports.signUpGet = (req,res) =>{
     res.render('singup'); 
 }; 
+
 
 
 module.exports.signUpPost = async (req, res) => {
@@ -23,8 +60,8 @@ module.exports.signUpPost = async (req, res) => {
 
         res.status(201).json(user); 
     } catch (error) {
-        console.log(error); 
-        res.status(400).send("internal error"); 
+        const errors = handleErrors(error);
+        res.status(400).json({ errors }); 
     }
 };
 
--- models/User.js
+++ models/User.js
@@ -1,20 +1,21 @@
-const moongose = require('mongoose'); 
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+  }
+});
 
-const userSchema = moongose.Schema({
-    email:{
-        type : String , 
-        required : true , 
-        unique : true , 
-        lowercase : true , 
-    }, 
-    password :{
-        type : String , 
-        required : true , 
-        minLength : 6 , 
-    },
-}); 
+const User = mongoose.model('user', userSchema);
 
-
-
-module.exports = moongose.model("User" , userSchema); 
+module.exports = User;
